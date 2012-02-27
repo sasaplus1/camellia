3b75dcc77d85f0bab1e62e8f6a1e518bdcc5c9a2
sasaplus1 <sasaplus1@gmail.com>
2012-02-27 14:01:12 +0900

    FindFileを追加

---
+++ core/findfile/FindFile.pas
@@ -0,0 +1,17 @@
+unit FindFile;
+
+interface
+
+uses
+  Types;
+
+function FindFiles(const Directory, FileExt: WideString): TWideStringDynArray;
+
+implementation
+
+function FindFiles(const Directory, FileExt: WideString): TWideStringDynArray;
+begin
+
+end;
+
+end.
